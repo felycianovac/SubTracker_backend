2e6306b771fec4d7a7366bea9210c9bdb91b0240
felycianovac <felicianovac4@gmail.com>
2025-05-27 14:15:04 +0300

    chore(init): import Spring Boot project with required dependecies

---
+++ src/main/java/com/subtracker/api/SubtrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.subtracker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubtrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubtrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subtracker/api/SubtrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subtracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubtrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
